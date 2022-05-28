435521a4067af7b38e1f8832a50b580f3bcffeba
Ocraftyone <Ocraftyone@users.noreply.github.com>
2022-05-28 03:46:27 -0400

    remove unnecessary code, optimize imports

--- src/main/java/com/ocraftyone/rebindnarrator/KeyBindHandler.java
+++ src/main/java/com/ocraftyone/rebindnarrator/KeyBindHandler.java
@@ -2,11 +2,7 @@ package com.ocraftyone.rebindnarrator;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.EditBox;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraftforge.client.ClientRegistry;
-import net.minecraftforge.client.settings.IKeyConflictContext;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.client.settings.KeyModifier;
 
