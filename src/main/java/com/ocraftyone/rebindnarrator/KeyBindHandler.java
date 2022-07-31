ece98d210f4b2871ae67b6410bd7230a711129e2
Ocraftyone <Ocraftyone@users.noreply.github.com>
2022-07-31 05:26:06 -0400

    1.17.1 version

--- src/main/java/com/ocraftyone/rebindnarrator/KeyBindHandler.java
+++ src/main/java/com/ocraftyone/rebindnarrator/KeyBindHandler.java
@@ -2,9 +2,9 @@ package com.ocraftyone.rebindnarrator;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.ClientRegistry;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.client.settings.KeyModifier;
+import net.minecraftforge.fmlclient.registry.ClientRegistry;
 
 public final class KeyBindHandler {
     public static final KeyMapping TOGGLE_NARRATOR = new KeyMapping("key.rebindnarrator.togglenarrator", KeyConflictContext.UNIVERSAL, KeyModifier.CONTROL, InputConstants.Type.KEYSYM, InputConstants.KEY_B, KeyMapping.CATEGORY_MISC);
