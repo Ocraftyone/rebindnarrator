10697697a2e427ce2966723de84de6fe66215387
Ocraftyone <Ocraftyone@users.noreply.github.com>
2022-07-31 06:19:17 -0400

    1.16.x version

--- src/main/java/com/ocraftyone/rebindnarrator/KeyBindHandler.java
+++ src/main/java/com/ocraftyone/rebindnarrator/KeyBindHandler.java
@@ -1,13 +1,14 @@
 package com.ocraftyone.rebindnarrator;
 
-import com.mojang.blaze3d.platform.InputConstants;
-import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.ClientRegistry;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.util.InputMappings;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.client.settings.KeyModifier;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import org.lwjgl.glfw.GLFW;
 
 public final class KeyBindHandler {
-    public static final KeyMapping TOGGLE_NARRATOR = new KeyMapping("key.rebindnarrator.togglenarrator", KeyConflictContext.UNIVERSAL, KeyModifier.CONTROL, InputConstants.Type.KEYSYM, InputConstants.KEY_B, KeyMapping.CATEGORY_MISC);
+    public static final KeyBinding TOGGLE_NARRATOR = new KeyBinding("key.rebindnarrator.togglenarrator", KeyConflictContext.UNIVERSAL, KeyModifier.CONTROL, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_B, "key.categories.misc");
     
     public static void registerKeybinds() {
         ClientRegistry.registerKeyBinding(TOGGLE_NARRATOR);
--- src/main/java/com/ocraftyone/rebindnarrator/mixin/KeyboardHandlerMixin.java
+++ src/main/java/com/ocraftyone/rebindnarrator/mixin/KeyboardHandlerMixin.java
@@ -1,23 +1,29 @@
 package com.ocraftyone.rebindnarrator.mixin;
 
 import com.ocraftyone.rebindnarrator.KeyBindHandler;
-import net.minecraft.client.KeyboardHandler;
+import net.minecraft.client.KeyboardListener;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.ControlsScreen;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(KeyboardHandler.class)
+@Mixin(KeyboardListener.class)
 public abstract class KeyboardHandlerMixin {
+    @Shadow @Final private Minecraft minecraft;
+    
     @ModifyConstant(method = "keyPress", constant = @Constant(intValue = 66))
     private int getNarratorKey(int constant) {
         return KeyBindHandler.TOGGLE_NARRATOR.getKey().getValue();
     }
 
-    @Redirect(method = "keyPress", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;hasControlDown()Z", ordinal = 1))
+    @Redirect(method = "keyPress", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/Screen;hasControlDown()Z", ordinal = 1))
     private boolean getModifierKey() {
-        if (KeyBindHandler.TOGGLE_NARRATOR.isUnbound()) {
+        if (KeyBindHandler.TOGGLE_NARRATOR.isUnbound() || minecraft.screen instanceof ControlsScreen) {
             return false;
         }
         return KeyBindHandler.TOGGLE_NARRATOR.getKeyModifier().isActive(null);
